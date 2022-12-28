06e54b5fc135ac71e4614b7f0e80f0ef4a7fb220
choodoss <82578557+choodoss@users.noreply.github.com>
2022-12-28 11:09:16 +0000

    Deploying to gh-pages from @ choodoss/project-group-5@71952a1759ce06363dfef3d7132ae59e229a4f94 🚀

---
+++ index.67fa2bc4.js
@@ -0,0 +1,2 @@
+!function(){var e={openMenuBtn:document.querySelector("[data-menu-open]"),menu:document.querySelector("[data-menu]"),body:document.querySelector("body")};e.openmenuBtn.addEventListener("click",(function(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}))}();
+//# sourceMappingURL=index.67fa2bc4.js.map
