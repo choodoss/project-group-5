ba1696184965c114e2bd8cc4c6c6acb86f153917
Дмитрий Хоменко <82578557+choodoss@users.noreply.github.com>
2022-12-28 14:35:49 +0200

    fix menu

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,14 +1,18 @@
 (() => {
     const refs = {
         openMenuBtn: document.querySelector("[data-menu-open]"),
+        closeMenuBtn: document.querySelector("[data-menu-close]"),
         menu: document.querySelector("[data-menu]"),
+        btn: document.querySelector("[btn-menu]"),
         body: document.querySelector("body"),
     };
 
-    refs.openmenuBtn.addEventListener("click", toggleModal);
+    refs.openMenuBtn.addEventListener("click", toggleModal);
+    refs.closeMenuBtn.addEventListener("click", toggleModal);
 
     function toggleModal() {
         refs.menu.classList.toggle("is-hidden");
         refs.body.classList.toggle("no-scroll");
+        refs.btn.classList.toggle("change");
     }
 })();
