8db746e3c39fffde7e865501d1c39fc088152e99
Дмитрий Хоменко <82578557+choodoss@users.noreply.github.com>
2022-12-28 15:08:13 +0200

    fix

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,7 +1,6 @@
 (() => {
     const refs = {
         openMenuBtn: document.querySelector("[data-menu-open]"),
-        closeMenuBtn: document.querySelector("[data-menu-close]"),
         menu: document.querySelector("[data-menu]"),
         btn: document.querySelector("[btn-menu]"),
         body: document.querySelector("body"),
