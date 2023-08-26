42de307785e831208d045b532905302f81b01311
Dmytro Khomenko <82578557+choodoss@users.noreply.github.com>
2023-08-26 09:43:02 +0300

    fix header

--- src/js/mob-menu.js
+++ src/js/mob-menu.js
@@ -1,16 +1,14 @@
 (() => {
     const refs = {
-        openMenuBtn: document.querySelector("[data-menu-open]"),
         menu: document.querySelector("[data-menu]"),
         btn: document.querySelector("[btn-menu]"),
         body: document.querySelector("body"),
     };
 
-    refs.openMenuBtn.addEventListener("click", toggleModal);
-    refs.closeMenuBtn.addEventListener("click", toggleModal);
+    refs.btn.addEventListener("click", toggleModal);
 
     function toggleModal() {
-        refs.menu.classList.toggle("is-hidden");
+        refs.menu.classList.toggle("close-menu");
         refs.body.classList.toggle("no-scroll");
         refs.btn.classList.toggle("change");
     }
---
+++ src/js/slider.js
@@ -0,0 +1,19 @@
+$('.autoplay-customers').slick({
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 5000,
+});
+
+// section gallery
+
+$('.autoplay-gallery').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  arrows: false,
+  autoplay: true,
+  autoplaySpeed: 6000,
+});
